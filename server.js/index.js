d994f523d32295c6e818b007daa2fa7228912957
Andrew <andrew.mk4@yahoo.com>
2024-03-09 02:59:24 -0800

    dependencies added

--- server.js/index.js
+++ server.js/index.js
@@ -1,16 +1,42 @@
-const path = require("path");
+require("dotenv").config();
+const { PORT, NODE_ENV = "development" } = process.env;
 const express = require("express");
+const serveIndex = require("serve-index");
+const cors = require("cors");
 
-const PORT = process.env.PORT || 3001;
+const morgan = require("morgan");
 
 const app = express();
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-// Handle GET requests to /api route
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from NewtFig!" });
+
+app.use(cors());
+
+app.use(morgan("tiny"));
+
+app.use((req, res, next) => {
+  console.log("Time: ", Date.now());
+  next();
 });
-// All other GET requests not handled before will return our React app
+
+app.use("/login", (req, res) => {
+  res.send({
+    token: "test234"
+  });
+});
+
+app.listen(8080, () => console.log("API is running on http://localhost:8080/login"));
+
+app.use("/request-type", (req, res, next) => {
+  console.log("Request type: ", req.method);
+  next();
+});
+
+app.use("/public", express.static("public"));
+app.use("/public", serveIndex("public"));
+
+app.get("/", (req, res) => {
+  res.send("Successful response.");
+});
+
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}/api`);
+  console.log(`Server listening on ${PORT}`);
 });
